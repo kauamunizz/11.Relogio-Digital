0a26a1eb6f40d9bd3885a743ec8a5feef4f070e6
Kauã Muniz <kauamunizz@hotmail.com>
2022-07-26 13:19:12 -0300

    correcao

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 'stric mode'
 
 const index = (() => {
-    setInterval(function tempo() {
-        let data = new Date();
-        let hora = data.getHours()
-        let minuto = data.getMinutes()
-        let segundo = data.getSeconds()
+    function init() {
+        setInterval(function tempo() {
+            const data = new Date();
+            let hora = data.getHours();
+            let minuto = data.getMinutes();
+            let segundo = data.getSeconds();
+    
+            if (hora < 10) hora ='0' + hora;
+            if (minuto < 10) minuto ='0' + minuto;
+            if (segundo < 10) segundo ='0' + segundo;
+            
+            document.querySelector('.horas').textContent = hora;
+            document.querySelector('.minutos').textContent = minuto;
+            document.querySelector('.segundos').textContent = segundo;
+        }, 1000);
+    }
 
-        if(hora < 10) hora ='0' + hora;
-        if(minuto < 10) minuto ='0' + minuto;
-        if(segundo < 10) segundo ='0' + segundo;
-        
-        document.querySelector('.horas').textContent = hora;
-        document.querySelector('.minutos').textContent = minuto;
-        document.querySelector('.segundos').textContent = segundo;
-    })
-
+    return {
+        init
+    };
 })()
 
-index
+index.init();
